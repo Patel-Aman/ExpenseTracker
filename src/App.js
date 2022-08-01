13be5f4e0965beb376d854464de740614dd46794
Aman Patel <patel.26@iitj.ac.in>
2022-08-02 01:20:17 +0530

    started expense tracker project from react template.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's get started.</h1>
+      <p>Welcome to react learning.</p>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
