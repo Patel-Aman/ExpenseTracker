df1b8fa22fc0afc6e80a90e250c0c5c7a61a2859
Aman Patel <patel.26@iitj.ac.in>
2022-08-03 20:21:32 +0530

    added bar trackers

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,20 @@
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: props.id
+        };
+        props.onAddExpense(expenseData);
+    }
+
+    return (
+        <div className='new-expense'>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> 
+        </div>
+    );
+}
+
+export default NewExpense; 
